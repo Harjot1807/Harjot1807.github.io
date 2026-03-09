df2e5192c04fb53706af2e2bc254b1a2698bf792
Harjot1807 <harjotsingh180708@gmail.com>
2026-03-09 15:23:19 -0600

    started mines game

--- array-object/sketch.js
+++ array-object/sketch.js
@@ -12,4 +12,28 @@ function setup() {
 
 function draw() {
   background(220);
+  drawField();
 }
+
+//draws the basic 5x5 grid
+function drawField() {
+  for (let i = 1; i <5; i++) {
+    line(width*i/5, 0, width*i/5, height);
+  }
+  for (let j = 1; j <5; j++) {
+    line(0, height*j/5, width, height*j/5);
+  }
+}
+
+//function that will track the money based on the box clicked
+function trackMoney(){
+  let money = 500;
+  if (mousePressed){}
+}
+
+//giving values and different colors to each gem and mine
+function giveValues() {
+
+}
+
+
