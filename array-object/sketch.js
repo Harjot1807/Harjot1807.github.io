c21dde027d491c9b4a3964bf54b71f16decc9350
Harjot1807 <harjotsingh180708@gmail.com>
2026-03-10 15:23:12 -0600

    made the array logic, including placing mines

--- array-object/sketch.js
+++ array-object/sketch.js
@@ -5,9 +5,16 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+const GRIDSIZE = 25;
+let no_of_mines = 3;
+let grid = [];
+let gameOver = false;
+let balance = 500;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  drawGrid();
 }
 
 function draw() {
@@ -15,25 +22,44 @@ function draw() {
   drawField();
 }
 
-//draws the basic 5x5 grid
+//draws the basic 5x5 grid (the lines)
 function drawField() {
-  for (let i = 1; i <5; i++) {
-    line(width*i/5, 0, width*i/5, height);
+  for (let i = 1; i < 5; i++) {
+    line(width * i / 5, 0, width * i / 5, height);
   }
-  for (let j = 1; j <5; j++) {
-    line(0, height*j/5, width, height*j/5);
+  for (let j = 1; j < 5; j++) {
+    line(0, height * j / 5, width, height * j / 5);
   }
 }
 
 //function that will track the money based on the box clicked
-function trackMoney(){
+function trackMoney() {
   let money = 500;
-  if (mousePressed){}
+  if (mousePressed) { }
 }
 
-//giving values and different colors to each gem and mine
-function giveValues() {
+//making the grid with actual symbols
+function drawGrid() {
+  grid = [];
+  gameOver = false;
 
+  for (let n = 0; n < GRIDSIZE; n++) {
+    grid.push({
+      numberID: n,
+      isMine: false,
+      revealed: false,
+    });
+  }
+
+  let minesPlaced = 0;
+  while (minesPlaced < no_of_mines) {
+    let chooseRandom = Math.floor(Math.random() * GRIDSIZE);
+
+
+    if (grid[chooseRandom].isMine === false) {
+      grid[chooseRandom].isMine = true;
+      minesPlaced++;
+    }
+  }
 }
-
 
